a51cd92da014632823898eefab067069b48893f6
Marie Udam <mudam@klass2-18-l.intra.itcollege.ee>
2017-02-11 16:11:51 +0200

    CSS ja PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<h1>Tere!</h1>
+		<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		Tere, tere :)!
+		<a href="http://facebook.com/marie.udam">Minu FB profiil</a>
+		<img src="pics/pilt.jpg" alt="Pilt raudteega">
+
+<?php
+// prints e.g. 'Current PHP version: 4.1.1'
+echo 'Current PHP version: ' . phpversion();
+
+// prints e.g. '2.0' or nothing if the extension isn't enabled
+echo phpversion('tidy');
+?>
+	</body>
+</html>
